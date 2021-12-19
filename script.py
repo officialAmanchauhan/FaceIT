7f346e23aeeba205c23e6657064481e70a6484c3
Aman <uccamanchauhan2000@gmail.com>
2021-12-19 17:51:16 +0530

    detecting video files

--- script.py
+++ script.py
@@ -1,14 +1,47 @@
 #!/usr/bin/python3
 import cv2
-def faceDetect():
-    face_cascade = cv2.CascadeClassifier('frontfacepythonfile.xml')
-    img = cv2.imread('monalisa.jpg')
+
+"""
+OpenCV is an Open Source Computer Vision Library used for machine learning for performing real-time operations
+With OpenCV, you can do many things
+-> Face Detection
+-> Face Recognition
+-> Object Recognition
+-> Motion Tracking
+-> Mobile Robotics
+-> Statistical machine learning 
+and many more functionalities and can be used for security, emergency , verification , advance ticketing systems and many many more purposes
+OpenCV has many pre-trained classifiers for face, eyes, smile , etc on https://github.com/opencv/opencv/tree/master/data/haarcascades
+"""
+
+def faceDetect():                                                       # Defining function faceDetect
+
+    face_finder = cv2.CascadeClassifier('frontfacepythonfile.xml')
+    
+    """
+    Cascade Clasifier is used to detect object in images
+    I have used front face classifier  and specified it as frontfacepythonfile.xml
+    """
+    
+    img = cv2.imread('monalisa.jpg')                # Reads the image from the file specified by filename, Here monalisa.jpg
  
-    faces = face_cascade.detectMultiScale(img, 1.1, 4)
+    faces = face_finder.detectMultiScale(img, 1.1, 4)  
+    
+    """
+    This detect faces with three arguments (image , scale factor, Minneighbors )
+    Scale factor specifies how much image size is reduced with each scale
+    MinNeighbors specifies how many neighbors each candidate rectangle should have to retain it general is around 4-5
+    Note: Here, I didn't convert image to grayscale. I found OpenCV can detect faces from still images and can be done same with videos
+    if theya re converted to frames first that i have done in script2
+    """
 
     for (x, y, w, h) in faces: 
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 10)
+        
+        """ 
+        Now we are drawing a rectangle to the detected faces with coordinates x,y,w & h with RBG value (255,0,0)(Blue) with thickness !0
+        """
     
-    return cv2.imwrite("monalisaface_detected.png", img) 
+    return cv2.imwrite("monalisaface_detected.png", img)             # Returning value as image ( it's optional we can get image without calling our function back , it is just I Like it)
 
-faceDetect()
+faceDetect()                                 # Calling faceDetect Function to detect faces in given samples
---
+++ script2.py
@@ -0,0 +1,38 @@
+import cv2  
+  
+# Load the cascade  
+face_cascade = cv2.CascadeClassifier('frontfacepythonfile.xml')  
+  
+# To capture video from existing video , here happykid.mp4 is the resource file   
+vid = cv2.VideoCapture('happykid.mp4')         
+""" 
+Here we used a video source since it is also a collection of many frames combined
+You can also use your webcam to detect your face just replace ('happykid.mp4') to (0)
+"""
+
+while True:                   #Running a loop to read all the frames with flags & frames as _,img
+    # Reads the frame  
+    _, img = vid.read()  
+
+    # Detect the faces  
+    faces = face_cascade.detectMultiScale(img, 1.1, 4)  
+  
+    # Draw the rectangle around each face  
+    for (x, y, w, h) in faces:  
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)  
+  
+    # Display  
+    cv2.imshow('Video', img)  
+  
+    # Since it is a never ending loop , you can stop it if escape key is pressed  from this code written below
+    k = cv2.waitKey(1) 
+    if k==27:  
+        break  
+    
+    """ 
+    Here, waitkey is a function that allows user to display a window for a given set of time
+    """
+
+# Release the VideoCapture object  
+vid.release()  
+
